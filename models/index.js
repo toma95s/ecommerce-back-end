188072c0735949412780bb3b5910977ed519b4b0
toma <toma95s@tomas-MBP.lan>
2022-05-05 09:53:40 -0700

    added product tags, belongsto methods, model imports

--- models/index.js
+++ models/index.js
@@ -3,15 +3,29 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { getEnabledCategories } = require('trace_events');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey:'category_id'
+})
 
 // Categories have many Products
-
+Category.hasMany(Product,{
+  foreignKey:'category_id'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag,{
+  through: {
+    model: ProductTag
+  }
+})
 // Tags belongToMany Products (through ProductTag)
-
+  Tag.belongsToMany(Product,{
+    throuh: {
+      model: ProductTag
+    }
+  })
 module.exports = {
   Product,
   Category,
