1fab9310078dff887e23a9baf470fd0e7d266582
toma <toma95s@tomas-MBP.lan>
2022-05-05 09:41:53 -0700

    added product models

--- models/productTag.js
+++ models/productTag.js
@@ -1,12 +1,34 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      }
+
+    },
+    tag_id:{
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      }
+      
+    },
   },
   {
     sequelize,
